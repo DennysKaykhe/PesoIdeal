543d55cb3607efe9c0c4f527796d19498a4ecf60
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-16 21:40:12 -0300

    Eu amo alturas

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿double Altura, PesoIdeal;
+string Sexo;
+
+Console.Clear();
+
+Console.Write("Digite sua altura em m..........: ");
+Altura = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Sexo [M]asculino / [F]eminino...: ");
+Sexo = Console.ReadLine()!.Trim().ToUpper();
+
+if (Sexo != "M" && Sexo != "F")
+{
+    Console.WriteLine("Sexo inválido!");
+    return;
+}
+else
+{
+    if (Sexo == "M")
+    {
+        PesoIdeal = Altura * 72.7 - 58.0;
+    }
+    else
+    {
+        PesoIdeal = Altura * 62.1 - 44.7;
+    }
+
+    Console.WriteLine($"Seu peso ideal é {PesoIdeal:N1}kg.");
+}
